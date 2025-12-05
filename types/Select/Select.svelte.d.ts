4f3bcf1c3823359848eaa5bcc130fefcb9a5eba7
spburtsev <spburtsev@proton.me>
2025-12-05 20:27:24 +0100

    fix(time-picker-select)!: change labelText slot to labelChildren

--- types/Select/Select.svelte.d.ts
+++ types/Select/Select.svelte.d.ts
@@ -125,8 +125,7 @@ export default class Select<
 > extends SvelteComponentTyped<
   SelectProps<Value>,
   {
-    /** The selected value. */
-    update: CustomEvent<Value>;
+    /** The selected value. */ update: CustomEvent<Value>;
     change: WindowEventMap["change"];
     input: WindowEventMap["input"];
     focus: WindowEventMap["focus"];
